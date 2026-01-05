3b3dc179cd59b332f5fb6fade18669e4731c8c2b
TheGuy491 <tech@tkeener.com>
2026-01-04 23:02:28 -0500

    update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from stats import get_chars_dict
 from stats import dict_to_list
 
 
-if len(sys.argv) < 2:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
+
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book_path = sys.argv[1]
+    
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
